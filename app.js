de080388dd3c8ab21932ae243044e2393b855a6d
kevinh11 <kevinhadinata11@gmail.com>
2021-06-27 20:57:38 +0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,35 @@
+let toggle = document.getElementById("toggle-button")
+let toggle_div = document.getElementById("toggle-div")
+
+let annually = false
+function changePrice(annual) {
+    monthlyprice = [19.99, 24.99, 39.99]
+    annualprice = [199.99, 249.99, 399.99]
+    prices = document.getElementsByClassName("price")
+    for(i=0; i< prices.length; i ++) {
+        if (annual) {
+            prices[i].innerHTML = "$" + annualprice[i].toString()
+        
+        }
+        else {
+            prices[i].innerHTML = "$" + monthlyprice[i].toString()
+        }
+    
+    }
+    
+}
+toggle.addEventListener('click', function() {
+    if (!annually) {
+        toggle.style.paddingLeft = "40px";
+        toggle.style.opacity = "0.8";
+        changePrice(true)
+        annually = true;
+
+    }
+    else {
+        toggle.style.paddingLeft = "5px";
+        toggle.style.opacity = "1"
+        changePrice(false)
+        annually = false;
+    }
+})
